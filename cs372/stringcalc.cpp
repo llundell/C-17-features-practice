65fb92302f9551117765846de85bcd640c440ded
llundell <llundell@ymail.com>
2019-02-15 15:50:56 -0900

    add catch2.hpp

---
+++ cs372/stringcalc.cpp
@@ -0,0 +1,16 @@
+#define CATCH2_CONFIG_MAIN
+#include "catch.hh"
+
+
+
+
+#include <iostream>
+using std::cout;
+
+#include <string>
+using std::string;
+
+double stringCalc(string n)
+{
+	return 0;
+}
